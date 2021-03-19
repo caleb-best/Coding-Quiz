96ceabd0f83e687e9fb6f72049473ec808863e89
github <calbest31@outlook.com>
2021-03-19 00:09:39 -0400

    questions in javascript

---
+++ assets/js/script.js
@@ -0,0 +1,45 @@
+//Questions Array
+var questions = [
+    {
+        question: 'The condition in an if / else statement is enclosed with _______.',
+        answerA: '1. quotes',
+        answerB: '2. curly brackets',
+        answerC: '3. parenthesis',
+        answerD: '4. square brackets',
+        correctAnswer: 'c'
+    },
+    {
+        question: 'Arrays in JavaScript can be used to store _______.',
+        answerA: '1. numbers and strings',
+        answerB: '2. other arrays',
+        answerC: '3. booleans',
+        answerD: '4. all of the above',
+        correctAnswer: 'd'
+    },
+    {
+        question: 'String values must be enclosed within _______ when being assigned to variables.',
+        answerA: '1. commas',
+        answerB: '2. curly brackets',
+        answerC: '3. quotes',
+        answerD: '4. parenthesis',
+        correctAnswer: 'c'
+    },
+    {
+        question: 'A very useful tool used during development and debugging for printing content to the debugger is:',
+        answerA: '1. JavaScript',
+        answerB: '2. terminal/bash',
+        answerC: '3. for loops',
+        answerD: '4. console.log',
+        correctAnswer: 'd'
+    },
+    {
+        question: 'Commonly used data types DO NOT include:',
+        answerA: '1. strings',
+        answerB: '2. booleans',
+        answerC: '3. alerts',
+        answerD: '4. numbers',
+        correctAnswer: 'c'
+    }
+];
+
+console.log(questions)
